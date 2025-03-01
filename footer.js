9b031e051844867d4cde173b8590be0b873716f0
Satsuki-Amagori <satsuki.amagori@gmail.com>
2025-03-01 19:50:25 +0900

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const footerHTML = `
+        <div class="footer">
+            <a href="/wikipedia/index.html">トップページへ戻る</a>
+        </div>
+    `;
+    document.body.insertAdjacentHTML("beforeend", footerHTML);
+});
